e6f1a5f8672595902a35ffa770d29aae7ad6a8d2
vagrant <vagrant@localhost.localdomain>
2020-05-05 23:14:14 +0000

    [Add]index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,16 @@
 class BooksController < ApplicationController
   def index
+  	@books = Book.all.order(id: "ASC")
+  end
+
+  def new
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.find(params[:id])
+  	book.save
+  	redirect_to book_path
   end
 
   def show
@@ -7,4 +18,5 @@ class BooksController < ApplicationController
 
   def edit
   end
+
 end
